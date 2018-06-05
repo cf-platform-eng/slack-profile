794ca4b74f900a70ebf3f7eb451358d983ab82e4
JaredGordon <jgordon@pivotal.io>
2018-06-05 14:16:36 -0400

    got past challenge and see changes.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,33 +1,34 @@
 package hello;
 
-import org.springframework.http.MediaType;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class HelloController {
-    
+
     @RequestMapping("/")
     public String index() {
         return "Greetings from Space!";
     }
 
-    @RequestMapping(method = RequestMethod.POST, path="/")
-    public Map<String, String> token(@RequestBody Map<String,String> input) {
+    @RequestMapping(method = RequestMethod.POST, path = "/")
+    public Map<String, Object> token(@RequestBody Map<String, Object> input) {
 
         if (input != null && input.containsKey("challenge")) {
-            System.out.println(input.get("challenge"));
+            log.info(input.get("challenge").toString());
             input.remove("token");
             input.remove("type");
             return input;
         }
         // assume this is event info
         else {
-            System.out.println(input);
+            log.info("got a change!!!! " + input.toString());
             return null;
         }
 
